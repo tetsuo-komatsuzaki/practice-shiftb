3921ba0d043641834b2d74d24f605d09512f7f4c
tetsuo-komatsuzaki <tetsuo9293@gmail.com>
2025-11-02 22:10:57 +0900

    課題の実装３

--- src/post.jsx
+++ src/post.jsx
@@ -1,9 +1,22 @@
-import { posts } from "./data/posts";
+// import { posts } from "./data/posts";
 import './App.css';
 import PostItem from "./components/potItem/postItem";
 import Header from "./components/header/header";
+import { posts_URL } from './data/posts';
+
+import { useEffect, useState} from "react";
 
 export default function Post() {
+  const [posts,setPosts] = useState([]);
+
+useEffect(() =>{
+  const fetcher = async() =>{
+    const res = await fetch(`${posts_URL}/posts`)
+    const {posts} = await res.json();
+    setPosts(posts)
+  }
+  fetcher()
+},[])
 
   return (
 
