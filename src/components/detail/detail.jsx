db53c6c227855ace7ad6beffb83a2b440703afb0
tetsuo-komatsuzaki <tetsuo9293@gmail.com>
2025-11-02 22:09:16 +0900

    作業途中の変更を保存

--- src/components/detail/detail.jsx
+++ src/components/detail/detail.jsx
@@ -1,31 +1,52 @@
-import Header from "../header/header";
 import { useParams } from "react-router-dom";
 import classes from "./detail.module.css";
-import { posts } from "../../data/posts";
+import { posts_URL } from "../../data/posts";
+import { useState, useEffect } from "react";
 
 export default function Detail() {
   const { id } = useParams();
-  const postDetail = posts.find(p => String(p.id) === id)
-  const date = new Date(postDetail.createdAt)
+  const [postsDetail, setPostDetail] = useState(null);
+  const [loading,setLoading] = useState(false)
+
+
+  useEffect(() => {
+      const fetcher = async () => {
+        setLoading(true)
+        const res = await fetch(`${posts_URL}/posts/${id}`)
+        const {post} = await res.json()
+        setPostDetail(post)
+        setLoading(false)
+  };
+  fetcher()
+  }, [id]);
+if(loading){
+  return <div>読み込み中...</div>
+}
+
+  if (!loading && !postsDetail) {
+    return <div>記事が見つかりません</div>
+  }
+
+
 
   return (
     <>
       <div>
-        <img src={postDetail.thumbnailUrl} alt="" className={classes.thumbnail}/>
+        <img src={postsDetail.thumbnailUrl} alt="" className={classes.thumbnail} />
       </div>
       <div className={classes.article}>
         <div className={classes.meta}>
-          <span>{date.toLocaleDateString()}</span>
+          <span>{new Date(postsDetail.createdAt).toLocaleDateString()}</span>
           <span>
-            {postDetail.categories.map((text, index) => {
+            {postsDetail.categories.map((text, index) => {
               return (
                 <span className="categories" key={index}>{text}</span>
               )
             })}
           </span>
         </div>
-        <h1>{`APIで取得した${postDetail.title}`}</h1>
-        <div dangerouslySetInnerHTML={{ __html:postDetail.content }}></div>
+        <h1>{`APIで取得した${postsDetail.title}`}</h1>
+        <div dangerouslySetInnerHTML={{ __html: postsDetail.content }}></div>
       </div>
     </>
   )
