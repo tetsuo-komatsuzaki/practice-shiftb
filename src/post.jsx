3fde30d3a57ddd094f363cc61ac79231e6c83112
tetsuo-komatsuzaki <tetsuo9293@gmail.com>
2025-11-03 09:17:44 +0900

    作業途中の変更を保存

--- src/post.jsx
+++ src/post.jsx
@@ -8,15 +8,25 @@ import { useEffect, useState} from "react";
 
 export default function Post() {
   const [posts,setPosts] = useState([]);
+  const [loading,setLoading] = useState(false)
 
 useEffect(() =>{
   const fetcher = async() =>{
+    setLoading(true)
     const res = await fetch(`${posts_URL}/posts`)
     const {posts} = await res.json();
     setPosts(posts)
+    setLoading(false)
   }
   fetcher()
 },[])
+if(loading){
+  return <div>読み込み中...</div>
+}
+
+if(!loading&&!posts){
+  return <div>記事が見つかりません</div>
+}
 
   return (
 
